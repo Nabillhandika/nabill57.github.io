eea9ebbf5ff3b6c73ceab4e28112778624f09423
Nabillhandika <nabillhandika57@gmail.com>
2026-01-18 22:17:33 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Mengambil elemen tombol
+const tombol = document.getElementById('btnContact');
+
+// Menambahkan event klik
+tombol.addEventListener('click', () => {
+    alert('Terima kasih telah berkunjung! Email saya: nabillhandika@gmail.com');
+});
+
+// Efek konsol untuk memastikan JS jalan
+console.log("Web Native Nabill Berhasil Dimuat!");
